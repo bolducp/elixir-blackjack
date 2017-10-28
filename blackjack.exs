7fc4b9f699e2eabb3289956654d957c178e64438
Paige Bolduc <paigebolduc@gmail.com>
2017-10-28 10:47:44 -0400

    Initial commit
    All the credit to Dave :-)

---
+++ blackjack.exs
@@ -0,0 +1,99 @@
+defmodule Deck do
+  @ranks ~w(Ace 2 3 4 5 6 7 8 9 10 Jack Queen King)
+  @suits ~w(Clubs Diamonds Hearts Spades)
+
+  def new do
+    @suits
+    |> Enum.map(&iterate_over_ranks/1)
+    |> List.flatten
+  end
+
+  def deal_hand(deck) do
+    deal_hands(deck, 1)
+  end
+
+  def deal_hands(deck, num_hands) do
+    {deck, hands} = deal_one_card_to_hands(deck, make_hands(num_hands, []), [])
+    deal_one_card_to_hands(deck, hands, [])
+  end
+
+  ### PRIVATE STUFF
+
+  defp make_hands(0, acc), do: acc
+  defp make_hands(num_hands, acc), do: make_hands(num_hands - 1, [[]|acc])
+
+  defp deal_one_card_to_hands(deck, [], acc), do: {deck, acc |> Enum.reverse}
+  defp deal_one_card_to_hands([card|deck], [hand|rest], acc) do
+    deal_one_card_to_hands(deck, rest, [[card|hand]|acc])
+  end
+
+  defp iterate_over_ranks(suit) do
+    @ranks |> Enum.map(&(Card.from_rank_and_suit(&1, suit)))
+  end
+
+end
+
+defmodule Hand do
+  def value(hand) do
+    map = hand |> Enum.group_by(fn(c) -> elem(c,0) == "Ace" end)
+    aces = map[true] || []
+    others = map[false] || []
+    others_val = others |> Enum.map(&Card.value/1) |> Enum.sum
+    value_with_aces(length(aces), others_val)
+  end
+
+  def take_turns(deck, hands, acc \\ [])
+  def take_turns(deck, [], acc), do: {deck, acc |> Enum.reverse}
+  def take_turns(deck=[card|rest], [hand|others], acc) do
+    if value(hand) > 16 do
+      take_turns(deck, others, [hand|acc])
+    else
+      take_turns(rest, [[card|hand]|others], acc)
+    end
+  end
+
+  def winner(hands) do
+    # TODO: account for ALL busting
+    hands |> Enum.map(&value/1) |> Enum.reject(&(&1 > 21)) |> Enum.max
+  end
+
+  defp value_with_aces(0, others_val), do: others_val
+  defp value_with_aces(num_aces, others_val) do
+    val_as_ones = others_val + num_aces
+    # there is never room for two or more aces!
+    if val_as_ones <= 11, do: val_as_ones + 10, else: val_as_ones
+  end
+end
+
+defmodule Card do
+
+  @value_map %{
+    "Ace" => 11,
+    "2" => 2,
+    "3" => 3,
+    "4" => 4,
+    "5" => 5,
+    "6" => 6,
+    "7" => 7,
+    "8" => 8,
+    "9" => 9,
+    "10" => 10,
+    "Jack" => 10,
+    "Queen" => 10,
+    "King" => 10,
+  }
+
+  def ace_value_with(value_of_rest) do
+    cond do
+      value_of_rest > 11 -> 1
+      true               -> 11
+    end
+  end
+  def from_rank_and_suit(rank, suit) do
+    {rank, suit}
+  end
+
+  def value({rank, _suit}) do
+    @value_map[rank]
+  end
+end
---
+++ blackjack_test.exs
@@ -0,0 +1,89 @@
+Code.load_file("blackjack.exs", __DIR__)
+ExUnit.start
+ExUnit.configure trace: true
+
+
+defmodule BlackjackTest do
+  use ExUnit.Case
+
+  test "create a deck" do
+    deck = Deck.new
+    assert length(deck) == 52
+  end
+
+  test "deal a hand" do
+    deck = Deck.new
+    {rest, [hand]} = Deck.deal_hand(deck)
+    assert length(hand) == 2
+    assert length(rest) == 50
+    assert Enum.reverse(hand) ++ rest == deck
+  end
+
+  test "deal hands to multiple players" do
+    deck = Deck.new
+    num_hands = 3
+    {rest, hands} = Deck.deal_hands(deck, num_hands)
+    assert Enum.count(hands) == num_hands
+    hands |> Enum.each(fn(h) -> assert Enum.count(h) == 2 end)
+    assert length(rest) == 52 - (num_hands * 2)
+    assert take_back(List.to_tuple(hands), num_hands - 1, []) ==
+      deck |> Enum.take(num_hands * 2)
+  end
+
+  test "counts aces as 11 if room just barely allows" do
+    hand = [{"Ace", "Hearts"}, {"King", "Clubs"}]
+    assert Hand.value(hand) == 21
+  end
+
+  test "counts aces as 1 if need just barely be" do
+    hand = [{"7", "Diamonds"}, {"4", "Clubs"}, {"Ace", "Hearts"}]
+    assert Hand.value(hand) == 12
+  end
+
+  test "lets users take turns" do
+    deck = Deck.new
+    num_hands = 3
+    {deck, hands} = Deck.deal_hands(deck, num_hands)
+    Enum.each(hands, fn(h) -> refute Hand.value(h) > 15 end)
+    {deck, hands} = Hand.take_turns(deck, hands)
+    Enum.each(hands, fn(h) -> assert Hand.value(h) > 15 end)
+  end
+
+  test "gives users fresh cards" do
+    deck = Deck.new
+    num_hands = 3
+    {deck, hands} = Deck.deal_hands(deck, num_hands)
+    {deck, hands} = Hand.take_turns(deck, hands)
+    [hand1,hand2,hand3] = hands
+    refute Enum.any?(hand1, fn(card) -> Enum.member?(hand2, card) end)
+    refute Enum.any?(hand2, fn(card) -> Enum.member?(hand3, card) end)
+    refute Enum.any?(hand3, fn(card) -> Enum.member?(hand1, card) end)
+end
+
+  test "determines single winner" do
+    hands = [
+      [{"8", "Clubs"}, {"7", "Clubs"}, {"4", "Clubs"}, {"Ace", "Clubs"}],  # 20
+      [{"10", "Clubs"}, {"9", "Clubs"}, {"5", "Clubs"}, {"2", "Clubs"}],   # 26
+      [{"Jack", "Clubs"}, {"6", "Clubs"}, {"3", "Clubs"}]  # 19
+    ]
+    assert Hand.winner(hands) == 20
+  end
+
+  test "declares winners if tie" do
+    # TODO: WRITE THIS
+  end
+
+  test "declares no winner if all bust" do
+    # TODO: WRITE THIS
+  end
+
+  defp take_back(hands, which, acc) do
+    case elem(hands, which) do
+      [] -> acc
+      [card|rest] ->
+        next = if which == 0, do: tuple_size(hands) - 1, else: which - 1
+        take_back(put_elem(hands, which, rest), next, [card|acc])
+    end
+  end
+
+end
